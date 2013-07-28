ccf5fbe17a84ba29f19d5beb1ed03471d8b951f6
Simon Corless <simon@sico.co.uk>
2013-07-28 23:11:56 +0100

    Initial commit of existing code

---
+++ lang/English/sico_disable_styles.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('FORUM')) die();
+
+$lang_sico_disable_styles = array(
+    'Label update style' => 'Update User Styles',
+    'Description update style' => 'If checked all user styles will automatically be updated to the selected style.',
+);
+?>
